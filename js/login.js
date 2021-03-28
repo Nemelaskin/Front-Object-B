e86d341e7be4a4486a565a7867f0c7b15cfbf581
Nemelas <49837703+Nemelaskin@users.noreply.github.com>
2021-03-28 20:46:47 +0300

    auth

---
+++ js/login.js
@@ -0,0 +1,57 @@
+function setCookie(name, value, options = {}) {
+
+    options = {
+        path: '/',
+        ...options
+    };
+
+    if (options.expires instanceof Date) {
+        options.expires = options.expires.toUTCString();
+    }
+
+    let updatedCookie = name + "=" + value;
+
+    for (let optionKey in options) {
+        updatedCookie += "; " + optionKey;
+        let optionValue = options[optionKey];
+        if (optionValue !== true) {
+            updatedCookie += "=" + optionValue;
+        }
+    }
+
+    document.cookie = updatedCookie;
+}
+
+var test;
+async function postAuth(){
+    
+    var data = {
+        Email : Email.value,
+        Password : Password.value
+    }
+
+    var response;
+    response = await fetch('http://localhost:5000/api/Auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(data)
+      });
+      test = (await response.json()).access_token;
+      
+      console.log(test);
+      //console.log(dad);
+      
+    
+    
+      /*response(token => {
+        localStorage.setItem(ACCESS_TOKEN_KEY, token.access_token);
+    });*/
+
+}
+
+function authOkey(token){
+    setCookie('JWT', token, { 'max-age': 3600, samesite: 'strict' });
+    location.href = "../html/User/home.html";
+}
